d8f048d16dcd007fbc2db00acbe365faad102e72
Jackson <railsmail42@gmail.com>
2012-04-14 23:21:51 -0500

    Did I say Ruby? I meant Coffeescript.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,34 +1,5 @@
 #!/usr/bin/env ruby
-# -*- coding: UTF-8 -*-
+# encoding: utf-8
 # This file is part of paradiddle <http://github.com/jacksonwillis/paradiddle/>.
 
-require "bundler"
-begin
-  Bundler.setup(:default, :development, :test)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
-require "jeweler"
-Jeweler::Tasks.new do |gem|
-  gem.name = "paradiddle"
-  gem.homepage = "http://github.com/jacksonwillis/paradiddle"
-  gem.license = "Tiny"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
-  gem.authors = ["Jackson Willis"]
-  gem.executables = "paradiddle"
-  gem.required_ruby_version = ">= 1.9.2"
-end
-Jeweler::RubygemsDotOrgTasks.new
-
-$LOAD_PATH.unshift(File.expand_path("../lib", __FILE__))
-require "paradiddle"
-
-require "rspec/core"
-require "rspec/core/rake_task"
-RSpec::Core::RakeTask.new(:spec) { |spec| spec.pattern = FileList["spec/**/*_spec.rb"] }
-
-task :default => :spec
+task :default {}
